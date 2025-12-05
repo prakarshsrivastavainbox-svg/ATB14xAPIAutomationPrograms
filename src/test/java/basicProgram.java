1c96a69c395edbc4f744c3268b9a6e5fd8c10897
Prakarsh Srivastava <prakarshsrivastava.inbox@gmail.com>
2025-12-06 00:33:34 +0530

    Initial push

--- src/test/java/basicProgram.java
+++ src/test/java/basicProgram.java
@@ -1,8 +1,6 @@
-package com.myfirstrestassured;
-
 import io.restassured.RestAssured;
 
-public class helloworld {
+public class basicProgram {
     public static void main(String[] args) {
         RestAssured.given().baseUri("https://www.google1.com/").log().all().when().get().then().log().all();
     }
